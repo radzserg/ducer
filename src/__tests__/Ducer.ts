2244fced660cbe324172789a0749d611dcb61baf
Sergey Radzishevskyi <radzserg@gmail.com>
2022-01-28 12:50:06 +0300

    feat: add async story

--- src/__tests__/Ducer.ts
+++ src/__tests__/Ducer.ts
@@ -63,4 +63,29 @@ let p: PaidProject = scenarios.paidProject({
       createdAt: new Date("2022-02-02"),
     });
   });
+
+  it("adds async simple story to the bag", async () => {
+    const producer: Ducer = new Ducer();
+    producer.addStory(
+      "user",
+      async (userData: Partial<UserInput>): Promise<User> => {
+        return {
+          ...{
+            id: 123,
+            firstName: "John",
+            lastName: "Doe",
+            createdAt: new Date("2022-02-02"),
+          },
+          ...userData,
+        };
+      }
+    );
+    const user = await producer.make("user", { firstName: "Tom", lastName: "Lee" });
+    expect(user).toEqual({
+      id: 123,
+      firstName: "Tom",
+      lastName: "Lee",
+      createdAt: new Date("2022-02-02"),
+    });
+  });
 });
