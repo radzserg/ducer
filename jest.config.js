9d01d9c9acd8f9e140024cc7cc122217641d721c
Sergey Radzishevskii <sergey.radzishevskii@contra.com>
2022-07-19 11:05:07 +0300

    fix: remove tsd

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    modulePaths: ["<rootDir>/tests/"],
-    testPathIgnorePatterns: ["dist"],
-    // setupFilesAfterEnv: ["<rootDir>/jestFilesAfterEnv.ts"],
-};
